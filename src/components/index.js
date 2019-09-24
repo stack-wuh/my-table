e19e2679c960d4cdce686699980347190ec99883
shadow <wuh131420@foxmail.com>
2019-09-24 16:51:49 +0800

    2019-09-24-[build][更新打包配置][shadow]

---
+++ src/components/index.js
@@ -0,0 +1,21 @@
+import Table from './table/index.js'
+import TableHeader from './table-header/index'
+const components = [
+  Table,
+  TableHeader
+]
+
+const install = function (Vue) {
+  components.forEach(component => {
+    Vue.component(component.name, componnet)
+  })
+}
+if (typeof window !== 'undefined' && window.Vue) {
+  install(window.Vue)
+}
+
+export default  {
+  version: '0.0.1',
+  WTable: Table,
+  WTableHeader: TableHeader
+}
